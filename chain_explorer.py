a221adc315c5a1d835bef06c142caa43f0a4d054
Loki <lanky441@gmail.com>
2023-06-09 14:58:54 -0400

    Update chain_explorer.py to take chain_folder as argument and check whether chain_1.txt or chain_1.0.txt is present in that folder.

--- chain_explorer.py
+++ chain_explorer.py
@@ -4,20 +4,29 @@ import corner
 import json
 import argparse
 import os
+import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--chain_file", default="all_chains/chains_earth/chain_1.txt")
+parser.add_argument("-c", "--chain_folder", default="all_chains/chains_earth/")
 parser.add_argument("-noise", "--plot_noise_params", action='store_true')
 parser.add_argument("-psrterm", "--psrterm", action='store_true')
+parser.add_argument("-b", "--burn_fraction", default=4)
 
 
 args = parser.parse_args()
-chain_file = args.chain_file
+chain_folder = args.chain_folder
 plot_noise_params = args.plot_noise_params
 psrterm = args.psrterm
+burn_frac = args.burn_fraction
 
+if os.path.isfile(f"{chain_folder}/chain_1.txt"):
+    chain_file = f"{chain_folder}/chain_1.txt"
+elif os.path.isfile(f"{chain_folder}/chain_1.0.txt"):
+    chain_file = f"{chain_folder}/chain_1.0.txt"
+else:
+    sys.exit("Could not find any chain file in the chain folder! Exiting!")
 
-chain_folder = "/".join(chain_file.split("/")[:-1]) + "/"
+# chain_folder = "/".join(chain_file.split("/")[:-1]) + "/"
 param_names = np.genfromtxt(f"{chain_folder}/params.txt", dtype=str)
 psrlist = np.genfromtxt(f"{chain_folder}/psrlist.txt", dtype=str)
 noise_params_med = json.load(open(f"data/noise_param_median_5f.json", "r"))
@@ -26,7 +35,7 @@ npsr = len(psrlist)
 chain = np.loadtxt(chain_file)
 print(chain.shape)
 
-burn = chain.shape[0] //2
+burn = chain.shape[0] //burn_frac
 
 
 if plot_noise_params:
